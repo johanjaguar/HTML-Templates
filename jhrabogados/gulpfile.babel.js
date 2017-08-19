63d534fe4c8c7c229e602f201bb21ad4495df74a
johanjaguar <wormwoodster@gmail.com>
2017-08-18 23:12:13 -0500

    first steps to the jhr page

---
+++ jhrabogados/gulpfile.babel.js
@@ -0,0 +1,35 @@
+//variables con los plugins que usaremos
+var gulp = require('gulp');
+var util = require('gulp-util');
+var requireDir = require('require-dir');
+var browserSync = require('browser-sync');
+
+requireDir('./gulp-tasks');
+
+gulp.task( 'default',
+  ['browser-sync'], function(){
+    gulp.watch('**/*.scss',
+      {cwd: 'src/scss'} ,
+      ['sass']);
+    gulp.watch(  '**/*.js',
+      {cwd: 'src/javascript'} ,
+      ['scripts']);
+    gulp.watch('src/views/**/*.pug',
+      {cwd: 'src/views'} ,
+      ['pug']);
+    gulp.watch('index.pug',
+      {cwd: ''} ,
+      ['pug']);
+    gulp.watch([ '**/*.gif' , '**/*.png' , '**/*.jpg' , '**/*.jpeg' ]
+      , {cwd: 'src/images'} ,
+      ['imagesmin']
+    );
+    gulp.watch(['css/**/*.css', 'js/**/*.js'],
+      {cwd: 'build'} ,
+      ['bs-reload']
+    );
+    gulp.watch("index.html",
+      {cwd: ''} ,
+      ['bs-reload']
+    );
+});
