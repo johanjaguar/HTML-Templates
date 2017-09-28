94d0a832d8a3812ad6d22959fcead407f5d31aa0
Johan Vargas <johanvargas@outlook.com>
2017-09-27 19:32:17 -0500

    jaguares template

---
+++ jaguares/build/js/final.js
@@ -0,0 +1,2 @@
+"use strict";
+//# sourceMappingURL=final.js.map
---
+++ jaguares/gulp-tasks/pug.js
@@ -0,0 +1,10 @@
+const gulp = require('gulp'),
+	pug = require('gulp-pug');
+
+gulp.task('pug', () =>
+	gulp.src('index.pug')
+		.pipe(pug({
+			pretty: true
+		}))
+		.pipe(gulp.dest(''))
+);
